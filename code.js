2e793f0d150a071cd0201c3a93d980fcde0306fa
asuslaptop95439 <90461341+asuslaptop95439@users.noreply.github.com>
2021-09-11 20:57:09 +0200

    Update code.js

--- code.js
+++ code.js
@@ -5,6 +5,8 @@ const app = express();
 app.use(express.json());
 app.use(express.static("public"));
 
+const port= process.env.PORT || 3000;
+
 
 app.get("/",function(request,response){
 	response.sendFile(__dirname + "public/index.html");
